9a946f40b94b9693be39273b1eab31f39257ac48
Chetan Dhandre <149818574+Chetandhandre21@users.noreply.github.com>
2025-05-09 11:45:13 +0530

    Add files via upload

---
+++ Car-Rental-Java-DBMS-Project-master/Car-Rental-Java-DBMS-Project-master/src/com/mysql/jdbc/PreparedStatement.java
@@ -0,0 +1,31 @@
+package com.mysql.jdbc;
+
+import java.sql.ResultSet;
+
+public class PreparedStatement {
+
+	public void setString(int i, String usrname) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+	public ResultSet executeQuery() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+	public void executeUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+
+}
---
+++ Car-Rental-Java-DBMS-Project-master/Car-Rental-Java-DBMS-Project-master/src/com/toedter/calendar/JDateChooser.java
@@ -0,0 +1,25 @@
+package com.toedter.calendar;
+
+public class JDateChooser {
+
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setEnabled(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setDateFormatString(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void cleanup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
